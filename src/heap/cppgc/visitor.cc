acc8a2a673c66d36cd8da7d4ad368464a620ebc1
Mingle Chen <minglechen2002@gmail.com>
2024-10-26 22:49:37 +0800

    [cppgc] Only skip pointers when not compressed.
    Ensure that we check that tag only when pointer compression is off.

--- src/heap/cppgc/visitor.cc
+++ src/heap/cppgc/visitor.cc
@@ -44,7 +44,7 @@ void ConservativeTracingVisitor::TraceConservatively(
     MSAN_MEMORY_IS_INITIALIZED(&maybe_full_ptr, sizeof(maybe_full_ptr));
 #endif
     // First, check the full pointer.
-#if defined(__CHERI_PURE_CAPABILITY__)
+#if defined(__CHERI_PURE_CAPABILITY__) && !defined(CPPGC_POINTER_COMPRESSION)
     if (!__builtin_cheri_tag_get(maybe_full_ptr)) continue;
 #endif
     if (maybe_full_ptr > SentinelPointer::kSentinelValue)
