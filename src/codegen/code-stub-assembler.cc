3747180c10cc54e4bbca104f53efac225d5d9018
Domagoj Stolfa <ds815@cam.ac.uk>
2024-11-30 14:56:58 +0000

    [codegen] Remove explicit CapAdds from CSA.
    This was breaking mksnapshot on the compressed build and is no longer
    necessary on the uncompressed build.

--- src/codegen/code-stub-assembler.cc
+++ src/codegen/code-stub-assembler.cc
@@ -5110,11 +5110,7 @@ void CodeStubAssembler::FillFixedArrayWithSmiZero(ElementsKind kind,
       FixedArray::kHeaderSize - kHeapObjectTag;
   TNode<IntPtrT> offset =
       ElementOffsetFromIndex(start, kind, fa_base_data_offset);
-#if defined(__CHERI_PURE_CAPABILITY__)
-  TNode<WordT> backing_store = CapAdd(BitcastTaggedToWord(array), offset);
-#else   // !__CHERI_PURE_CAPABILITY__
   TNode<IntPtrT> backing_store = IntPtrAdd(BitcastTaggedToWord(array), offset);
-#endif  // __CHERI_PURE_CAPABILITY__
 
   // Call out to memset to perform initialization.
   TNode<ExternalReference> memset =
@@ -5149,11 +5145,7 @@ void CodeStubAssembler::FillFixedDoubleArrayWithZero(
       FixedDoubleArray::kHeaderSize - kHeapObjectTag;
   TNode<IntPtrT> offset = ElementOffsetFromIndex(start, PACKED_DOUBLE_ELEMENTS,
                                                  fa_base_data_offset);
-#if defined(__CHERI_PURE_CAPABILITY__)
-  TNode<WordT> backing_store = CapAdd(BitcastTaggedToWord(array), offset);
-#else   // !__CHERI_PURE_CAPABILITY__
   TNode<IntPtrT> backing_store = IntPtrAdd(BitcastTaggedToWord(array), offset);
-#endif  // __CHERI_PURE_CAPABILITY__
 
   // Call out to memset to perform initialization.
   TNode<ExternalReference> memset =
