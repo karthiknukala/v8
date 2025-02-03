ea59cbe3bae45341010ad95ee499740793e6f239
Domagoj Stolfa <ds815@cam.ac.uk>
2025-02-03 23:11:09 +0000

    [codegen] Allow calling SmiUntag with C registers.
    Since Smis will never really be capabilities, but we can end up in this
    code path from the machine-independent baseline assembler, simply force
    them to be X registers here.

--- src/codegen/arm64/macro-assembler-arm64-inl.h
+++ src/codegen/arm64/macro-assembler-arm64-inl.h
@@ -1367,15 +1367,17 @@ void MacroAssembler::SmiTag(Register dst, Register src) {
 void MacroAssembler::SmiTag(Register smi) { SmiTag(smi, smi); }
 
 void MacroAssembler::SmiUntag(Register dst, Register src) {
+#ifndef __CHERI_PURE_CAPABILITY__
   DCHECK(dst.Is64Bits() && src.Is64Bits());
+#endif  // !__CHERI_PURE_CAPABILITY__
   if (v8_flags.enable_slow_asserts) {
     AssertSmi(src);
   }
   DCHECK(SmiValuesAre32Bits() || SmiValuesAre31Bits());
   if (COMPRESS_POINTERS_BOOL) {
-    Sbfx(dst, src.W(), kSmiShift, kSmiValueSize);
+    Sbfx(dst.X(), src.W(), kSmiShift, kSmiValueSize);
   } else {
-    Asr(dst, src, kSmiShift);
+    Asr(dst.X(), src.X(), kSmiShift);
   }
 }
 
