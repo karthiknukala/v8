ed3346eb04d905a8a250126346f71ae2ec75db5f
Domagoj Stolfa <ds815@cam.ac.uk>
2025-06-18 15:23:55 +0100

    [freebsd] Remove an unnecessary ifdef.
    This should always be a static_cast, so just treat it as that.

--- src/base/platform/platform-freebsd.cc
+++ src/base/platform/platform-freebsd.cc
@@ -82,13 +82,8 @@ std::vector<OS::SharedLibraryAddress> OS::GetSharedLibraryAddresses() {
             lib_name = std::string(path);
           }
           result.push_back(SharedLibraryAddress(
-#if defined(__CHERI_PURE_CAPABILITY__)
               lib_name, static_cast<uintptr_t>(map->kve_start),
               static_cast<uintptr_t>(map->kve_end)));
-#else    // !__CHERI_PURE_CAPABILITY__
-              lib_name, reinterpret_cast<uintptr_t>(map->kve_start),
-              reinterpret_cast<uintptr_t>(map->kve_end)));
-#endif   // !__CHERI_PURE_CAPABILITY__
         }
 
         start += ssize;
