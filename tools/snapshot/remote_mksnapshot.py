bea11f04cdd579fe84665e7db246b74d6be2db5c
RoundofThree <zhuoyingjiangli@gmail.com>
2025-08-05 16:24:22 +0100

    [LICENSE] Add myself

--- tools/snapshot/remote_mksnapshot.py
+++ tools/snapshot/remote_mksnapshot.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+# Copyright 2025 CTSRD-CHERI. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 """
 Copy necessary files to run mksnapshot into a remote host, execute mksnapshot,
 then copy the results back out. This script reads all the possible input and
