d5eab183d6bdb8fa188b6bbeba38ef86fb3cd452
Domagoj Stolfa <ds815@cam.ac.uk>
2025-11-07 18:45:02 +0000

    [run-kraken] Disable a currently broken benchmark.
    This will have to be re-enabled at some point in the future once it's
    fixed.

--- test/benchmarks/csuite/run-kraken.js
+++ test/benchmarks/csuite/run-kraken.js
@@ -29,7 +29,9 @@
 // http://hg.mozilla.org/projects/kraken/sunspider script.
 
 var benchmarkPath = "";
-var tests = [ "ai-astar", "audio-beat-detection", "audio-dft", "audio-fft", "audio-oscillator", "imaging-gaussian-blur", "imaging-darkroom", "imaging-desaturate", "json-parse-financial", "json-stringify-tinderbox", "stanford-crypto-aes", "stanford-crypto-ccm", "stanford-crypto-pbkdf2", "stanford-crypto-sha256-iterative" ];
+// FIXME(ds815): Re-enable the json-parse-financial benchmark once it's fixed.
+// var tests = [ "ai-astar", "audio-beat-detection", "audio-dft", "audio-fft", "audio-oscillator", "imaging-gaussian-blur", "imaging-darkroom", "imaging-desaturate", "json-parse-financial", "json-stringify-tinderbox", "stanford-crypto-aes", "stanford-crypto-ccm", "stanford-crypto-pbkdf2", "stanford-crypto-sha256-iterative" ];
+var tests = [ "ai-astar", "audio-beat-detection", "audio-dft", "audio-fft", "audio-oscillator", "imaging-gaussian-blur", "imaging-darkroom", "imaging-desaturate", "json-stringify-tinderbox", "stanford-crypto-aes", "stanford-crypto-ccm", "stanford-crypto-pbkdf2", "stanford-crypto-sha256-iterative" ];
 var categories = [ "ai", "audio", "imaging", "json", "stanford" ];
 var results = new Array();
 
